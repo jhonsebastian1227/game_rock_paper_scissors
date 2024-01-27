1cf3b4a57010c11a486b7d81c39b374767bc6b71
Jhon Sebastian <136397923+jhonsebastian1227@users.noreply.github.com>
2024-01-27 15:42:58 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# game_rock_paper_scissors
+Img:
+https://www.freepik.es/vector-gratis/coleccion-manos-diferentes-posturas-estilo-plano_2096135.htm#page=3&query=rock%20paper%20and%20scissors&position=38&from_view=search&track=ais&uuid=e70f63bc-076b-436c-8441-1cbacb510e3f
+
+Palette:
+https://colorhunt.co/palette/ffb534fbf6eec1f2b065b741
---
+++ assets/js/index.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var countPlayer = 0;
+    var countIA = 0;
+    
+    function changeColor(color){
+        element = document.getElementsByTagName("h2");
+        for(var i = 0; i < element.length; i++){
+            element[i].style.color = color;
+        }
+    }
+    
+    document.getElementById("myButton1").addEventListener("click", function(){
+        var randomNumberIA = Math.floor((Math.random() * 4) + 1);
+
+        document.getElementById("player").setAttribute("src", "./assets/images/rock.png");
+        document.getElementsByTagName
+        if(randomNumberIA == 1){
+            document.getElementById("IA").setAttribute("src", "./assets/images/rock.png");
+            changeColor("#FBF6EE");
+        }
+        else if(randomNumberIA == 2){
+            document.getElementById("IA").setAttribute("src", "./assets/images/paper.png");
+            countIA++;
+            document.getElementById("countIA").innerHTML = "Player IA: " + countIA;
+            changeColor("black")
+        }
+        else{
+            document.getElementById("IA").setAttribute("src", "./assets/images/scissors.png");
+            countPlayer++;
+            document.getElementById("countPlayer").innerHTML = "Player: " + countPlayer;
+            changeColor("#black")
+        }
+    });
+
+
+    document.getElementById("myButton2").addEventListener("click", function(){
+        var randomNumberIA = Math.floor((Math.random() * 4) + 1);
+
+        document.getElementById("player").setAttribute("src", "./assets/images/paper.png");
+        
+        if(randomNumberIA == 1){
+            document.getElementById("IA").setAttribute("src", "./assets/images/paper.png");
+            changeColor("#FBF6EE");
+        }
+        else if(randomNumberIA == 2){
+            document.getElementById("IA").setAttribute("src", "./assets/images/scissors.png");
+            countIA++;
+            document.getElementById("countIA").innerHTML = "Player IA: " + countIA;
+            changeColor("black")
+        }
+        else{
+            document.getElementById("IA").setAttribute("src", "./assets/images/rock.png");
+            countPlayer++;
+            document.getElementById("countPlayer").innerHTML = "Player: " + countPlayer;
+            changeColor("black")
+        }
+    });
+
+
+    document.getElementById("myButton3").addEventListener("click", function(){
+        var randomNumberIA = Math.floor((Math.random() * 4) + 1);
+
+        document.getElementById("player").setAttribute("src", "./assets/images/scissors.png");
+        
+        if(randomNumberIA == 1){
+            document.getElementById("IA").setAttribute("src", "./assets/images/scissors.png");
+            changeColor("#FBF6EE");
+        }
+        else if(randomNumberIA == 2){
+            document.getElementById("IA").setAttribute("src", "./assets/images/rock.png");
+            countIA++;
+            document.getElementById("countIA").innerHTML = "Player IA: " + countIA;
+            changeColor("black")
+        }
+        else{
+            document.getElementById("IA").setAttribute("src", "./assets/images/paper.png");
+            countPlayer++;
+            document.getElementById("countPlayer").innerHTML = "Player: " + countPlayer;
+            changeColor("black")
+        }
+    });
+});
