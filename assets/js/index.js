8cba944692ba58de644ce706defda70338bce64d
Jhon Sebastian <136397923+jhonsebastian1227@users.noreply.github.com>
2024-01-29 22:42:17 -0300

    index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,97 +1,101 @@
-// Variables de conteo para saber quien va ganando
-var countPlayer = 0;
-var countIA = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    // Código que se ejecutará cuando el DOM esté completamente cargado
 
-// Declaracion de variables para selecionar los botones
-var array = document.querySelectorAll(".myButton");
+    // Variables de conteo para saber quien va ganando
+    var countPlayer = 0;
+    var countIA = 0;
 
-// for para encontrar el boton pulsado entre los tres que hay
-for(var i = 0; i < array.length; i++){
-    array[i].addEventListener("click", function(){
-        // genera un numero aleatorio
-        var randomNumberIA = Math.floor((Math.random() * 4) + 1);
+    // Declaracion de variables para selecionar los botones
+    var array = document.querySelectorAll(".myButton");
 
-        // toma de deciones dependiendo de cual boton se pulso o el muero que salio
-        switch (this.innerHTML) {
-            case "Rock":
-                if(randomNumberIA == 1){
-                    changeAttribute("./assets/images/rock.png", "./assets/images/rock.png");
-                    changeColor("#FBF6EE");
-                }
-        
-                else if(randomNumberIA == 2){
-                    changeAttribute("./assets/images/rock.png", "./assets/images/paper.png");
-                    countIA++;
-                    functionCountIA(countPlayer, countIA);
-                }
-        
-                else if(randomNumberIA == 3){
-                    changeAttribute("./assets/images/rock.png", "./assets/images/scissors.png");
-                    countPlayer++;
-                    functionCountIA(countPlayer, countIA);
-                }
-                break;
+    // for para encontrar el boton pulsado entre los tres que hay
+    for(var i = 0; i < array.length; i++){
+        array[i].addEventListener("click", function(){
+            // genera un numero aleatorio
+            var randomNumberIA = Math.floor((Math.random() * 4) + 1);
+
+            // toma de deciones dependiendo de cual boton se pulso o el muero que salio
+            switch (this.innerHTML) {
+                case "Rock":
+                    if(randomNumberIA == 1){
+                        changeAttribute("./assets/images/rock.png", "./assets/images/rock.png");
+                        changeColor("#FBF6EE");
+                    }
             
-            case "Paper":
-                if(randomNumberIA == 1){
-                    changeAttribute("./assets/images/paper.png", "./assets/images/paper.png");
-                    changeColor("#FBF6EE");
-                }
-        
-                else if(randomNumberIA == 2){
-                    changeAttribute("./assets/images/paper.png", "./assets/images/scissors.png");
-                    countIA++;
-                    functionCountIA(countPlayer, countIA);
-                }
-        
-                else if(randomNumberIA == 3){
-                    changeAttribute("./assets/images/paper.png", "./assets/images/rock.png");
-                    countPlayer++;
-                    functionCountIA(countPlayer, countIA);
-                }
-                break;
+                    else if(randomNumberIA == 2){
+                        changeAttribute("./assets/images/rock.png", "./assets/images/paper.png");
+                        countIA++;
+                        functionCountIA(countPlayer, countIA);
+                    }
             
-            case "Scissors":
-                if(randomNumberIA == 1){
-                    changeAttribute("./assets/images/scissors.png", "./assets/images/scissors.png");
-                    changeColor("#FBF6EE");
-                }
-        
-                else if(randomNumberIA == 2){
-                    changeAttribute("./assets/images/scissors.png", "./assets/images/rock.png");
-                    countIA++;
-                    functionCountIA(countPlayer, countIA);
-                }
-        
-                else if(randomNumberIA == 3){
-                    changeAttribute("./assets/images/scissors.png", "./assets/images/paper.png");
-                    countPlayer++;
-                    functionCountIA(countPlayer, countIA);
-                }
-                break;
-        
-            default: console.log("end");
-                break;
-        } 
-    });
-}
-
-
-function changeAttribute(player, IA){
-    document.getElementById("player").setAttribute("src", player);
-    document.getElementById("IA").setAttribute("src", IA);
-}
-    
-
-function functionCountIA(valuePlayer, valueIA){
-    document.getElementById("countIA").innerHTML = "Player IA: " + valueIA;
-    document.getElementById("countPlayer").innerHTML = "Player: " + valuePlayer;
-    changeColor("black");
-}
-
-function changeColor(color){
-    element = document.getElementsByTagName("h2");
-    for(var i = 0; i < element.length; i++){
-        element[i].style.color = color;
+                    else if(randomNumberIA == 3){
+                        changeAttribute("./assets/images/rock.png", "./assets/images/scissors.png");
+                        countPlayer++;
+                        functionCountIA(countPlayer, countIA);
+                    }
+                    break;
+                
+                case "Paper":
+                    if(randomNumberIA == 1){
+                        changeAttribute("./assets/images/paper.png", "./assets/images/paper.png");
+                        changeColor("#FBF6EE");
+                    }
+            
+                    else if(randomNumberIA == 2){
+                        changeAttribute("./assets/images/paper.png", "./assets/images/scissors.png");
+                        countIA++;
+                        functionCountIA(countPlayer, countIA);
+                    }
+            
+                    else if(randomNumberIA == 3){
+                        changeAttribute("./assets/images/paper.png", "./assets/images/rock.png");
+                        countPlayer++;
+                        functionCountIA(countPlayer, countIA);
+                    }
+                    break;
+                
+                case "Scissors":
+                    if(randomNumberIA == 1){
+                        changeAttribute("./assets/images/scissors.png", "./assets/images/scissors.png");
+                        changeColor("#FBF6EE");
+                    }
+            
+                    else if(randomNumberIA == 2){
+                        changeAttribute("./assets/images/scissors.png", "./assets/images/rock.png");
+                        countIA++;
+                        functionCountIA(countPlayer, countIA);
+                    }
+            
+                    else if(randomNumberIA == 3){
+                        changeAttribute("./assets/images/scissors.png", "./assets/images/paper.png");
+                        countPlayer++;
+                        functionCountIA(countPlayer, countIA);
+                    }
+                    break;
+            
+                default: console.log("end");
+                    break;
+            } 
+        });
     }
-}
+
+
+    function changeAttribute(player, IA){
+        document.getElementById("player").setAttribute("src", player);
+        document.getElementById("IA").setAttribute("src", IA);
+    }
+        
+
+    function functionCountIA(valuePlayer, valueIA){
+        document.getElementById("countIA").innerHTML = "Player IA: " + valueIA;
+        document.getElementById("countPlayer").innerHTML = "Player: " + valuePlayer;
+        changeColor("black");
+    }
+
+    function changeColor(color){
+        element = document.getElementsByTagName("h2");
+        for(var i = 0; i < element.length; i++){
+            element[i].style.color = color;
+        }
+    }
+});
