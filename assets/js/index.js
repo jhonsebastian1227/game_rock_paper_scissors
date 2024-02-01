ae986382ed3f4fa23b66000a2179c53ca1843ffe
Jhon Sebastian <136397923+jhonsebastian1227@users.noreply.github.com>
2024-01-31 19:30:42 -0600

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Variables de conteo para saber quien va ganando
     var countPlayer = 0;
-    var countIA = 0;
+    var countComp = 0;
 
     // Declaracion de variables para selecionar los botones
     var array = document.querySelectorAll(".myButton");
@@ -12,64 +12,64 @@ document.addEventListener('DOMContentLoaded', function() {
     for(var i = 0; i < array.length; i++){
         array[i].addEventListener("click", function(){
             // genera un numero aleatorio
-            var randomNumberIA = Math.floor((Math.random() * 4) + 1);
+            var randomNumberComp = Math.floor((Math.random() * 4) + 1);
 
             // toma de deciones dependiendo de cual boton se pulso o el muero que salio
             switch (this.innerHTML) {
                 case "Rock":
-                    if(randomNumberIA == 1){
+                    if(randomNumberComp == 1){
                         changeAttribute("./assets/images/rock.png", "./assets/images/rock.png");
-                        changeColor("#FBF6EE");
+                        functionCount(countPlayer, countComp,  1, "#FBF6EE");
                     }
             
-                    else if(randomNumberIA == 2){
+                    else if(randomNumberComp == 2){
                         changeAttribute("./assets/images/rock.png", "./assets/images/paper.png");
-                        countIA++;
-                        functionCountIA(countPlayer, countIA);
+                        countComp++;
+                        functionCount(countPlayer, countComp, 2, "black");
                     }
             
-                    else if(randomNumberIA == 3){
+                    else if(randomNumberComp == 3){
                         changeAttribute("./assets/images/rock.png", "./assets/images/scissors.png");
                         countPlayer++;
-                        functionCountIA(countPlayer, countIA);
+                        functionCount(countPlayer, countComp, 3, "black");
                     }
                     break;
                 
                 case "Paper":
-                    if(randomNumberIA == 1){
+                    if(randomNumberComp == 1){
                         changeAttribute("./assets/images/paper.png", "./assets/images/paper.png");
-                        changeColor("#FBF6EE");
+                        functionCount(countPlayer, countComp, 1, "#FBF6EE");
                     }
             
-                    else if(randomNumberIA == 2){
+                    else if(randomNumberComp == 2){
                         changeAttribute("./assets/images/paper.png", "./assets/images/scissors.png");
-                        countIA++;
-                        functionCountIA(countPlayer, countIA);
+                        countComp++;
+                        functionCount(countPlayer, countComp, 2, "black");
                     }
             
-                    else if(randomNumberIA == 3){
+                    else if(randomNumberComp == 3){
                         changeAttribute("./assets/images/paper.png", "./assets/images/rock.png");
                         countPlayer++;
-                        functionCountIA(countPlayer, countIA);
+                        functionCount(countPlayer, countComp, 3, "black");
                     }
                     break;
                 
                 case "Scissors":
-                    if(randomNumberIA == 1){
+                    if(randomNumberComp == 1){
                         changeAttribute("./assets/images/scissors.png", "./assets/images/scissors.png");
-                        changeColor("#FBF6EE");
+                        functionCount(countPlayer, countComp, 1, "#FBF6EE");
                     }
             
-                    else if(randomNumberIA == 2){
+                    else if(randomNumberComp == 2){
                         changeAttribute("./assets/images/scissors.png", "./assets/images/rock.png");
-                        countIA++;
-                        functionCountIA(countPlayer, countIA);
+                        countComp++;
+                        functionCount(countPlayer, countComp, 2, "black");
                     }
             
-                    else if(randomNumberIA == 3){
+                    else if(randomNumberComp == 3){
                         changeAttribute("./assets/images/scissors.png", "./assets/images/paper.png");
                         countPlayer++;
-                        functionCountIA(countPlayer, countIA);
+                        functionCount(countPlayer, countComp, 3, "black");
                     }
                     break;
             
@@ -80,16 +80,25 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
-    function changeAttribute(player, playerIA){
+    function changeAttribute(player, playerComp){
         document.getElementById("player").setAttribute("src", player);
-        document.getElementById("IA").setAttribute("src", playerIA);
+        document.getElementById("comp").setAttribute("src", playerComp);
     }
         
 
-    function functionCountIA(valuePlayer, valueIA){
-        document.getElementById("countIA").innerHTML = "Player IA: " + valueIA;
+    function functionCount(valuePlayer, valueComp, decision, color){
+    	changeColor(color);
+        document.getElementById("countComp").innerHTML = "Comp: " + valueComp;
         document.getElementById("countPlayer").innerHTML = "Player: " + valuePlayer;
-        changeColor("black");
+        if(decision == 1){
+             document.getElementById("decision").innerHTML = "Draw";
+         }
+        else if(decision == 2){
+             document.getElementById("decision").innerHTML = "Comp wins";
+         }
+         else{
+         	document.getElementById("decision").innerHTML = "Player wins";
+         }
     }
 
     function changeColor(color){
